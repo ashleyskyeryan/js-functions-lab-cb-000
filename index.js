ee096c41889f255c4cc07adf91a7ddcc21d44cb9
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-18 22:44:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidaysTo(name, holiday) {
 }
 
 function holidayCountdown(days, holiday) {
-  return `It\'s ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
