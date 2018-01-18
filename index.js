6f32fd9582fb3e2a6b7373a48ae7a89e5895ae8a
Ashley Ryan <ashleyskyeryan@gmail.com>
2018-01-18 22:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ function happyHolidaysTo(name) {
 function happyHolidaysTo(name, holiday) {
   `Happy ${holiday}, ${name}!`
 }
+
+function holidayCountdown(days, holiday) {
+  `It's ${days} days until ${holiday}!`
+}
